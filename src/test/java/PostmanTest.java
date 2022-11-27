081a29a7438168d0781bc3affa1c19b6c8530065
Egor Boyko <leferchian@gmail.com>
2022-11-27 21:05:40 +0300

    drop Test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -17,7 +17,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("soe data"))
         ;
     }
 
@@ -31,7 +31,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("Информация"))
+                .body("data",equalTo("Инфомация"))
                 ;
     }
 
