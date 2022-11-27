e42cd6b6d3d3f23a1316254844e2e82cf2787026
Egor Boyko <leferchian@gmail.com>
2022-11-27 23:37:09 +0300

    reformat code

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -22,7 +22,7 @@ public class PostmanTest {
     }
 
     @Test
-    void testPostCyrillic(){
+    void testPostCyrillic() {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
@@ -31,8 +31,8 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("Инфомация"))
-                ;
+                .body("data", equalTo("Инфомация"))
+        ;
     }
 
 }
